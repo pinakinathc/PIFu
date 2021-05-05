834751fa2579e46130418b1083de9f0dc9eaa142
Pinaki Nath Chowdhury <pinakinathc@users.noreply.github.com>
2021-05-05 17:27:20 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -11,4 +11,4 @@ cd /vol/research/sketchcaption/extras/original/PIFu
 # Complete Experiment
 cp val.txt.bkp val.txt
 
-/user/HS500/pc00725/miniconda3/envs/PIFu/bin/python -m apps.train_shape --name=ndf_overall --dataroot=/vol/research/NOBACKUP/CVSSP/scratch_4weeks/pinakiR/extra/training_data/ --random_flip --random_scale --random_trans --random_multiview --num_views=3 --batch_size=1 --no_num_eval --no_gen_mesh --sigma=0.3 --num_threads=12 --load_netG_checkpoint_path=checkpoints/ndf_overall/netG_latest	
+/user/HS500/pc00725/miniconda3/envs/PIFu/bin/python -m apps.train_shape --name=ndf_overall --dataroot=/vol/research/NOBACKUP/CVSSP/scratch_4weeks/pinakiR/extra/training_data/ --random_flip --random_scale --random_trans --random_multiview --num_views=3 --batch_size=1 --no_num_eval --no_gen_mesh --sigma=0.3 --num_threads=12	
